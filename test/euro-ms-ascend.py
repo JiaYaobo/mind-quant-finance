96c3de5b80c8c1a246dd8685e7bb564bd8ef8910
Lu Weizheng <luweizheng36@hotmail.com>
2021-11-22 18:14:10 +0800

    euro ascend test

--- test/euro-ms-ascend.py
+++ test/euro-ms-ascend.py
@@ -22,11 +22,10 @@ class EuroCallPrice(nn.Cell):
         self.strike = strike
         self.maturity = maturity
 
-    def construct(self):
+    def construct(self, WTs):
         # 使用定义好的运算构建前向网络
-        std = Tensor(np.sqrt(self.maturity), mindspore.float32) 
         drift = 0.5 * self.volatility ** 2 * self.maturity
-        WTs = ops.normal((self.m,), Tensor(0.0, mindspore.float32), std)
+        # WTs = ops.normal((self.m,), Tensor(0.0, mindspore.float32), std)
         STs = ops.Mul()(Tensor(self.spot, mindspore.float32), ops.Exp()(ops.Add()(Tensor(-drift, mindspore.float32), ops.Mul()(Tensor(self.volatility, mindspore.float32), WTs))))
         payoffs = ops.Maximum()(Tensor(0.0, mindspore.float32), ops.Add()(STs, Tensor(-self.strike, mindspore.float32)))
         result = ops.ReduceMean(keep_dims=True)(payoffs)
@@ -35,19 +34,20 @@ class EuroCallPrice(nn.Cell):
 
 
 if __name__ == '__main__':
-	m = 1000000
-	spot = 276.10
-	volatility = 0.407530933
-	strike = 230
+    m = 1000000
+    spot = 276.10
+    volatility = 0.407530933
+    strike = 230
     maturity = 58 / 365
+    std = np.sqrt(maturity)
     context.set_context(device_target="Ascend", device_id=7)
-    
+    x = np.random.normal(0.0, std, (m, ))
     start_ts = time.time()
     model = EuroCallPrice(m, spot, volatility, strike, maturity)
-    result = model()
+    result = model(x)
     print(result)
     first = time.time()
 	# r = estimate_call_price_ms(m, spot, volatility, strike, maturity)
-    result = model()
-    
+    result = model(x)
+
     print(time.time() - start_ts)
